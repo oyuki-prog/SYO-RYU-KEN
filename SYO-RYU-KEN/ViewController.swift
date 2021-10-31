90b2719b285336e9969c15f89fd1fc89caa9cdce
oyuki-prog <oyu8no3jo@gmail.com>
2021-10-31 18:19:52 +0900

    Initial Commit

---
+++ SYO-RYU-KEN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SYO-RYU-KEN
+//
+//  Created by o.yuki on 2021/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
